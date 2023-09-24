ba6ab00e0895bd4747136e2e5117dc7a05b61f76
SebaMediza <sebamediza@hotmail.com>
2023-09-24 18:13:30 -0300

    refactor

--- TP04_GP01/TP02_GP01/src/main/java/com/umlpam/aydsII/tp02_gp01/Ejercicios.java
+++ TP04_GP01/TP02_GP01/src/main/java/com/umlpam/aydsII/tp02_gp01/Ejercicios.java
@@ -1,10 +1,11 @@
 package com.umlpam.aydsII.tp02_gp01;
-
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+*/
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
