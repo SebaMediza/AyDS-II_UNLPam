cc4b70aea67a5213bd046ea912ac23765c7b7329
SebaMediza <sebamediza@hotmail.com>
2023-10-02 01:21:40 -0300

    db y query

--- porky_v1/src/main/java/com/aydsii/porky/controlador.java
+++ porky_v1/src/main/java/com/aydsii/porky/controlador.java
@@ -1,6 +1,7 @@
 package com.aydsii.porky;
 
 import java.util.HashMap;
+import java.util.List;
 
 import spark.ModelAndView;
 import spark.Request;
@@ -8,11 +9,21 @@ import spark.Response;
 import spark.Route;
 import spark.template.velocity.VelocityTemplateEngine;
 
+@SuppressWarnings("unchecked")
 public class controlador {
-    public static Route indice = (Request request, Response response) ->{
+    public static Route indice = (Request request, Response response) -> {
         HashMap<String, String> model = new HashMap<>();
         model.put("template","template/indice.vsl");
-        System.out.println(producto.buscarProducto());
+        //System.out.println(producto.buscarProducto());
+        return new VelocityTemplateEngine().render(new ModelAndView(model, "template/layout.vsl"));
+    };
+    @SuppressWarnings("rawtypes")
+    public static Route allProductos = (Request request, Response response) -> {
+        HashMap model = new HashMap();
+        List<producto> RES = producto.buscarProducto();
+        //System.out.print(((producto) RES).getNombre());
+        model.put("RES", RES);
+        model.put("template","template/carta.vsl");
         return new VelocityTemplateEngine().render(new ModelAndView(model, "template/layout.vsl"));
     };
 }
--- porky_v1/src/main/java/com/aydsii/porky/producto.java
+++ porky_v1/src/main/java/com/aydsii/porky/producto.java
@@ -1,7 +1,6 @@
 package com.aydsii.porky;
 
 import java.util.List;
-
 import org.sql2o.Connection;
 import org.sql2o.Sql2oException;
 
@@ -14,14 +13,22 @@ public class producto {
     private int precio_vta;
     private int cant_porciones;
     private String descripcion_producto;
+    private byte[] imagen;
 
     public static List<producto> buscarProducto(){
-        String querryString = "SELECT * FROM PRODUCTO";
+        String querryString = "SELECT ID_PRODUCTO, NOMBRE, PRECIO_VTA, CANT_PORCIONES, DESCRIPCION_PRODUCTO FROM PRODUCTO";
         List<producto> querryResult = null;
         try (Connection connection = sql2oDAO.getSql2oDAO().open()){
             querryResult = connection.createQuery(querryString).executeAndFetch(producto.class);
         } catch (Sql2oException sql2oException) {
             System.out.println(sql2oException);
+        }
+    for(producto producto : querryResult){
+            System.out.println(producto.getId_producto());
+            System.out.println(producto.getNombre());
+            System.out.println(producto.getDescripcion_producto());
+            System.out.println(producto.getPrecio_vta());
+            System.out.println(producto.getCant_porciones());
         }
         return querryResult;
     }
