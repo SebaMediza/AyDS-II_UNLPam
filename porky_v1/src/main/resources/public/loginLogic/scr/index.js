28b42f0b57831c169cb0ad024631191c890e3688
SebaMediza <sebamediza@hotmail.com>
2023-10-25 10:37:26 -0300

    refactor

---
+++ porky_v1/src/main/resources/public/loginLogic/scr/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'
+
+const firebaseApp = initializeApp({
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyA9oQiRvbmh2VQHbNsYI_X_jpOx-r0FboU",
+    authDomain: "porky-cakes.firebaseapp.com",
+    projectId: "porky-cakes",
+    storageBucket: "porky-cakes.appspot.com",
+    messagingSenderId: "703799900814",
+    appId: "1:703799900814:web:2a6cba65fae6a2936e27df",
+    measurementId: "G-XYHH2TZ2Z9"
+});
