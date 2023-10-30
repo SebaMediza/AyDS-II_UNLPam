642cd59a0331d15287ded15d3e9fcc361a65f148
SebaMediza <sebamediza@hotmail.com>
2023-10-30 16:43:16 -0300

    auth

--- porky_v1/src/main/java/com/aydsii/porky/ControladorCliente.java
+++ porky_v1/src/main/java/com/aydsii/porky/ControladorCliente.java
@@ -9,6 +9,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 
 @SuppressWarnings("unchecked")
 public class ControladorCliente {
+    final static String adminUid = "Am0dLzqsvDVhyhhwLXbob4m484y2";
     @SuppressWarnings("rawtypes")
     public static Route indice = (Request request, Response response) -> {
         HashMap model = new HashMap();
@@ -16,9 +17,29 @@ public class ControladorCliente {
         return new VelocityTemplateEngine().render(new ModelAndView(model, "template/layout.vsl"));
     };
     @SuppressWarnings("rawtypes")
+    public static Route indiceLogeado = (Request request, Response response) -> {
+        HashMap model = new HashMap();
+        String layout = "";
+        String uid = request.queryParams("uid");
+        System.out.println(uid);
+        if(uid.matches(adminUid)){
+            layout = "template/layoutAdmin.vsl";
+        }else{
+            layout = "template/layout.vsl";
+        }
+        model.put("template","template\\indice.vsl");
+        return new VelocityTemplateEngine().render(new ModelAndView(model, layout));
+    };
+    @SuppressWarnings("rawtypes")
     public static Route logIn = (Request request, Response response) -> {
         HashMap model = new HashMap();
         model.put("template","template\\login.vsl");
+        return new VelocityTemplateEngine().render(new ModelAndView(model, "template/layout.vsl"));
+    };
+    @SuppressWarnings("rawtypes")
+    public static Route singUp = (Request request, Response response) -> {
+        HashMap model = new HashMap();
+        model.put("template","template\\registro.vsl");
         return new VelocityTemplateEngine().render(new ModelAndView(model, "template/layout.vsl"));
     };
 }
--- porky_v1/src/main/java/com/aydsii/porky/Main.java
+++ porky_v1/src/main/java/com/aydsii/porky/Main.java
@@ -9,13 +9,17 @@ import com.google.firebase.auth.FirebaseAuthException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, FirebaseAuthException {
+        FireBaseController.getFirestoreConnection();
         staticFiles.location("/public");
         //Lado del Cliente
         get("/home", ControladorCliente.indice);
+        get("/uhome", ControladorCliente.indiceLogeado);
         get("/productos", ProductoControlador.listarProductos);
         get("/producto", ProductoControlador.buscarNombre);
         get("/masinfo", ProductoControlador.masInformacion);
         get("/login", ControladorCliente.logIn);
+        get("/login", ControladorCliente.logIn);
+        get("/singup", ControladorCliente.singUp);
         //Lado del administrador
         get("/admin", ControladorAdmin.admin);
         get("/admin/productos", ControladorAdmin.adminProductos);
